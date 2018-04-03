69869fad3b12715011be03a32a39720ae88b26be
TaSiya <siyandamakhalima.1@gmail.com>
2018-04-03 17:00:02 +0200

    just added the function to calculate, still need to add function for the event listener

--- js/calculate-bill.js
+++ js/calculate-bill.js
@@ -1,13 +1,37 @@
+
 //get a reference to the calculate button
-
+var calculateBtnElement = document.querySelector('.calculateBtn');
 //get a reference to the billTotal element
-
+var totalElement = document.querySelector('.total');
 //get a reference to the billString
-
+var billStringElement = document.querySelector(".billStrng");
 //create the function that will be called when the calculate button is pressed
 //  * this function should read the string value entered - split it on a comma.
 //  * loop over all the entries in the the resulting list
 //  * check if it is a call or an sms and add the right amount to the overall total
 //  * once done looping over all the entries - display the total onto the screen in the billTotal element
 
+
 //link the function to a click event on the calculate button
+
+
+function calculateBtnClicked(){
+   var billList = billStringElement.split(', ');
+   var total = 0 ;
+
+   for(var i= 0 ; i < billList.length ; i++ ){
+      if(billList[i].startsWith('c')){
+         total += 2.75;
+      }
+      else if(billList[i].startsWith('s')){
+         total += 0.75;
+      }
+   }
+
+   totalElement = total.toFixed(2);
+   document.total = totalElement;
+}
+
+calculateBtnElement.addEventListener('click', calculateBtnClicked);{
+   
+}
