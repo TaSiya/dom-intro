d42fb8e4e61e732a2a6dce6fb40f94c32f62de18
TaSiya <siyandamakhalima.1@gmail.com>
2018-04-11 12:19:28 +0200

    fixed bugs in settings widget

--- js/settings-bill.js
+++ js/settings-bill.js
@@ -29,20 +29,19 @@ var sms = 0.00;
 var total = 0.00;
 //add an event listener for when the 'Update settings' button is pressed
 updateSettings.addEventListener('click', function(){
+
    callCost = parseFloat(callCostSetting.value);
    smsCost = parseFloat(smsCostSetting.value);
    warning = parseFloat(warningLevelSetting.value);
    critical = parseFloat(criticalLevelSetting.value);
+
+   color();
+
 });
 //add an event listener for when the add button is pressed
 settingBtnAdd.addEventListener('click', function(){
    settings();
-   if(total >= warning & total <critical){
-      totalSettings.classList.add("warning");
-   }
-   else if (total >= critical){
-   totalSettings.classList.add("danger");
-   }
+   color();
 });
 //in the event listener get the value from the billItemTypeRadio radio buttons
 // * add the appropriate value to the call / sms total
@@ -51,19 +50,56 @@ settingBtnAdd.addEventListener('click', function(){
 // * display the latest total on the screen.
 // * check the value thresholds and display the total value in the right color.
 
+function color(){
+   if(total <= warning){
+      totalSettings.classList.remove('warning');
+   }
+   if(total >= warning  & total <critical){
+      totalSettings.classList.add("warning");
+   }
+   else if (total >= critical){
+      totalSettings.classList.add("danger");
+   }
+}
+
 function settings(){
+
    var checkedRadioBtn = document.querySelector("input[name='billItemTypeWithSettings']:checked");
    if (checkedRadioBtn){
       var billItemSettings = checkedRadioBtn.value;
    }
-   if(billItemSettings === 'call'){
-      call = call + callCost;
+
+   if(total > critical){
+
    }
-   else if (billItemSettings === 'sms'){
-      sms = sms + smsCost;
+   else{
+      if(billItemSettings === 'call'){
+         call = call + callCost;
+      }
+      else if (billItemSettings === 'sms'){
+         sms = sms + smsCost;
+      }
+      
+      total = (call + sms).toFixed(2) ;
+      
+      if(total > critical){
+         if(billItemSettings === 'call'){
+            total -= callCost;
+            call -= callCost;
+            
+         }
+         else if (billItemSettings === 'sms'){
+            total -= smsCost;
+            sms -= smsCost;
+            
+         }
+      }
+      else{
+         callTotalSettings.textContent = call.toFixed(2);
+         smsTotalSettings.textContent = sms.toFixed(2) ;
+         totalSettings.textContent = total;
+      }
+
    }
-   total = call + sms ;
-   callTotalSettings.textContent = call.toFixed(2);
-   smsTotalSettings.textContent = sms.toFixed(2) ;
-   totalSettings.textContent = total.toFixed(2);
+
 }
